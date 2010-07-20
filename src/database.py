56241fc21974a672209c4fc0a2613bbb9b9210bf
Malex <malexprojects@gmail.com>
2010-07-20 18:04:16 +0200

    Fixed addslashed, minor improves

--- src/database.py
+++ src/database.py
@@ -1,13 +1,14 @@
 from stdio import File
 import sqlite3
 import atexit
+import re
 
 class SQL:
 	""" Parent class for generic SQL support """
 
 	@staticmethod
 	def addslashes(s):
-		return s.replace("\"","\\\"")
+		return re.sub("([\\\"\'\0)","\\\1",s)
 
 
 class MySQL(SQL):
@@ -24,7 +25,7 @@ class SQLite(SQL):
 		self._conn = sqlite3.connect(filename)
 		self.db = self._conn.cursor()
 
-	def query(self,q):
+	def raw_query(self,q):
 		self.db.execute(SQL.addslashes(q))
 		return self.db
 
@@ -36,10 +37,12 @@ class SQLite(SQL):
 
 class DB:
 
-	self.db_hash
+	self.db_hash = {}
 
-	def query(self,string):
+	def raw_query(self,string):
 		self.driver.query(string)
 
 	def __init__(self,db_type,filename="./data.db"):
 		self.driver = globals()[self.db_hash[db_type]](File.parse(filename))
+
+	#def __getitem__ <= how to do multi-level?
