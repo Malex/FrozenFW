5ea3d6859a9dc3e6b069f5c21f80b2a9ba367774
Malex <malexprojects@gmail.com>
2011-03-26 21:50:51 +0100

    wtf2

--- Makefile
+++
@@ -1,5 +0,0 @@
-PYTHON="python3.1"
-DIR="/home/malex/pymod/frozen"
-
-install:
-	cp -R src/* $(DIR)
---
+++ setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup
+
+setup(name="frozen",version="2.0-alpha",
+		packages=['frozen'],
+		package_dir={"frozen":"src"})
