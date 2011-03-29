3dc56b3cb218719d6f990342a99d35ff3bf4d97b
Malex <malexprojects@gmail.com>
2011-03-29 21:57:11 +0200

    Some changes, conf.py.example init

--- frozen/plugins/Err_Pages.py
+++ frozen/plugins/Err_Pages.py
@@ -3,7 +3,7 @@ def state(stat :str,head :Headers,body :str, filename :str) -> list:
 	if stat.startswith("200"):
 		return Response(stat,head,body,filename)
 	else:
-		body = File.get_contents(conf.query("{}-page".formar(stat[0:3])))
+		body = File.get_contents(conf.query("Err_pages")[stat[0:3]])
 		return Response(stat,head,body,filename,ready=True)
 
 dispatch += state
