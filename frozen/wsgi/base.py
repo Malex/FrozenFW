7ac2026e350efb7acb7ca710ea7140349046a0b9
Malex <malexprojects@gmail.com>
2011-04-08 17:56:58 +0200

    Fixes

--- frozen/wsgi/base.py
+++ frozen/wsgi/base.py
@@ -1,16 +1,16 @@
 import os
 
-from .Data import Data,DataError
-from .Cookie import Cookie,CookieError
-from .stdio import Output,print,output
-from .functions import unquote,nl2br,htmlspecialchars,htmlentities
-from .Conf import Conf,ConfError
-from .File import File,open,FileError
-from .Dispatcher import Dispatcher,Response
-from .Headers import Headers,Header
-from .Logger import Logger,NOTICE,WARNING,ERROR,LoggerException
-from .Sandbox import Sandbox,EXEC,IMPORT
-from .Plugins import Plugins
+from ..Data import Data,DataError
+from ..Cookie import Cookie,CookieError
+from ..stdio import Output,print,output
+from ..functions import unquote,nl2br,htmlspecialchars,htmlentities
+from ..Conf import Conf,ConfError
+from ..File import File,open,FileError
+from ..Dispatcher import Dispatcher,Response
+from ..Headers import Headers,Header
+from ..Logger import Logger,NOTICE,WARNING,ERROR,LoggerException
+from ..Sandbox import Sandbox,EXEC,IMPORT
+from ..Plugins import Plugins
 
 conf = Conf("/etc/frozenrc")
 
